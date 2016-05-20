6d5475dedd1d5e8e2073a392112ab9ed6fe5095c
marshkat <marshkat@umail.iu.edu>
2016-05-19 21:22:11 -0400

    Update sumdigit.py

--- sumdigit.py
+++ sumdigit.py
@@ -5,11 +5,11 @@
 def sumd(n):
     import math
     digit=math.floor(math.log(n,10))+1
-    sum = 0
+    s = 0
     curr=n
     i=1
     while i <=digit:
-        sum=sum+int(curr%(10**i)/(10**(i-1)))
+        s=s+int(curr%(10**i)/(10**(i-1)))
         curr=curr-curr%(10**i)
         i=i+1
-    return sum
+    return s
